6a5687ddfb1cf6cfe30f09642cdd9f64eaa44e31
rafalkosla101 <63898848+rafalkosla101@users.noreply.github.com>
2022-01-22 10:38:03 +0100

    Add files via upload

---
+++ Lab 14/app.py
@@ -0,0 +1,21 @@
+from textblob import TextBlob
+
+def hello(name):
+    output = f'Hello {name}'
+    return output
+
+
+def extract_sentiment(text):
+    text = TextBlob(text)
+
+    return text.sentiment.polarity
+
+def text_contain_word(word: str, text: str):
+    return word in text
+
+def bubbleSort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1] :
+                arr[j], arr[j+1] = arr[j+1], arr[j]
